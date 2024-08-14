d12fe67e36f069f01c3c198f1845081735f3d49f
Mickael Agniel <mika5883@gmail.com>
2024-08-14 11:54:51 +0200

    Début du P3

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("Login JS est chargé");
+    /*
+    étape 1 créer le HTML et CSS de la page login
+    étape 2 le fichier login HTML doit appeler le fichier login.js = console.log de la ligne 2 doit s'afficher dans la console
+    étape 3 coder en javascript quand je clique sur le bouton envoyer : console.log ("boutonCliquer")
+    étape 4 qd je clique sur le bouton envoyer, je fais un console.log de ce qui a été saisie dans le champ e-mail
+    */
+  });
