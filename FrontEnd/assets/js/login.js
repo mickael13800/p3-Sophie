b523c358c388a503ad71e66c1e946cfb65dfd1c3
Mickael Agniel <mika5883@gmail.com>
2024-08-23 17:02:02 +0200

    ajout boutons filtres via JS

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("Login JS est chargé");
-    /*
+  console.log("Login JS est chargé");
+  /*
     étape 1 créer le HTML et CSS de la page login
     étape 2 le fichier login HTML doit appeler le fichier login.js = console.log de la ligne 2 doit s'afficher dans la console
     étape 3 coder en javascript quand je clique sur le bouton envoyer : console.log ("boutonCliquer")
     étape 4 qd je clique sur le bouton envoyer, je fais un console.log de ce qui a été saisie dans le champ e-mail
+    étape 5 coté login.html rendre obligatoire le champ email et password
     */
-  });
-let form = document.querySelector("form")
+});
+let form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  let email = document.getElementById("email")
-  console.log (email.value)
-})
-
-fetch ("http://localhost:5678/api/categories").then(response => {
-  console.log(response)
-});
-fetch ("http://localhost:5678/api/works").then(response => {
-  console.log(response)
+  event.preventDefault();
+  let email = document.getElementById("email");
+  console.log(email.value);
 });
--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("script JS est chargé");
-  });
+document.addEventListener("DOMContentLoaded", async (event) => {
+  console.log("script JS est chargé");
+
+  //Récupération des catégories depuis l'API
+  const categories = await fetch("http://localhost:5678/api/categories");
+  const dataCategories = await categories.json();
+  console.log(dataCategories);
+  //Récupération des works depuis l'API
+  const works = await fetch("http://localhost:5678/api/works");
+  const dataWorks = await works.json();
+  console.log(dataWorks);
+  //Affichage chaque label catégorie avec boucle
+  for (let i = 0; i < dataCategories.length; i++) {
+    console.log(dataCategories[i].name);
+  }
+  //GENERER BOUTONS FILTRES EN JS SUR HTML
+  //Récupération de la div .filtre
+  let divFiltre = document.querySelector(".filtres");
+  //Création des boutons filtres + intégration dans le fichier HTML
+  let boutonTous = document.createElement("button");
+  boutonTous.innerText = "Tous";
+  boutonTous.classList.add("filtre");
+  divFiltre.appendChild(boutonTous);
+
+  for (let i = 0; i < dataCategories.length; i++) {
+    let boutonFiltre = document.createElement("button");
+    boutonFiltre.innerText = dataCategories[i].name;
+    boutonFiltre.classList.add("filtre");
+    divFiltre.appendChild(boutonFiltre);
+  }
+});
+/*convertir la response en JSON
+      stocker ce data dans une variable dataCategories
+      faire le console.log
+      faire une boucle sur dataCategories pour que le data affiche le console.log du label de chaque categorie
+      generer en JS a l'emplacement du DOM .filtres les button : le 1er button on choisi "tous" et utiliser les 
+      boucles pour generer les boutons suivant a partir du dataCategories*/
+
+/*marre de JS = CSS sur bouton class=filtre : CSS special quand le bouton a la class active
+dans index.html en mode login marge noire en haut et pas en logout : filtres masqué en login
+wording login devient logout*/
