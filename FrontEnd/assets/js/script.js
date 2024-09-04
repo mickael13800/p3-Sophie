ace898a74f61b03e1ae0d272b78c0a159214fa45
Mickael Agniel <mika5883@gmail.com>
2024-09-04 16:33:40 +0200

    MAJ modale

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -9,6 +9,11 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     document.body.classList.remove("mode-edit");
   }
 
+  //Déconnexion de l'utilisateur
+  const logoutUser = document.querySelector(".logout");
+  logoutUser.addEventListener("click", () => {
+    window.localStorage.removeItem("token");
+  });
   //fonction appel catégories depuis l'API
   async function getDataCategories() {
     const categories = await fetch("http://localhost:5678/api/categories");
@@ -80,21 +85,66 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         //Afficher les éléments filtrés
         const imgWorks = document.querySelectorAll(".gallery figure");
 
-        if (categoryId === "All") {
-          for (let i = 0; i < imgWorks.length; i++) {
-            // Affiche toutes les images
+        for (let i = 0; i < imgWorks.length; i++) {
+          if (
+            categoryId === "All" ||
+            imgWorks[i].getAttribute("data-id") === categoryId
+          ) {
+            // Affiche les images correspondant à "All" ou au filtre sélectionné
             imgWorks[i].style.display = "block";
+          } else {
+            // Masque les images qui ne correspondent pas
+            imgWorks[i].style.display = "none";
           }
-        } else {
-          for (let i = 0; i < imgWorks.length; i++) {
-            if (imgWorks[i].getAttribute("data-id") === categoryId) {
-              // Affiche les images avec le bon ID
-              imgWorks[i].style.display = "block";
-            } else {
-              // Masque les images qui ne correspondent pas
-              imgWorks[i].style.display = "none";
-            }
-          }
+        }
+      });
+
+      //MODALE
+      const modalContainer = document.querySelector(".modal-container");
+      const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+      modalTriggers.forEach((trigger) =>
+        trigger.addEventListener("click", (event) => {
+          event.preventDefault();
+          toggleModal();
+        })
+      );
+
+      function toggleModal() {
+        modalContainer.classList.toggle("active");
+      }
+
+      //Modale step 1
+      //Récupération des éléments pour la 1ère modale
+      const stepOne = document.querySelector(".step-one");
+      const galleryPhoto = document.querySelector(".photo");
+      const btnAddPhoto = document.querySelector(".add-photo");
+
+      getDataWorks().then((dataWorks) => {
+        //Création de la galerie photo
+        for (let i = 0; i < dataWorks.length; i++) {
+          //création d'une div pour chaque photo + icone corbeille
+          let photoContainer = document.createElement("div");
+          photoContainer.classList.add("photo-container");
+          //création de la galerie d'image
+          let imgModal = document.createElement("img");
+          imgModal.src = dataWorks[i].imageUrl;
+          imgModal.alt = dataWorks[i].title;
+          //création de l'icone corbeille
+          let trashIcon = document.createElement("i");
+          trashIcon.classList.add("fa-solid", "fa-trash-can");
+          trashIcon.classList.add("trash-icon");
+
+          photoContainer.appendChild(imgModal);
+          photoContainer.appendChild(trashIcon);
+          galleryPhoto.appendChild(photoContainer);
+          stepOne.appendChild(galleryPhoto);
+          stepOne.appendChild(btnAddPhoto);
+
+          // Ajout d'un événement pour supprimer la photo au clic sur l'icône
+          trashIcon.addEventListener("click", function () {
+            photoContainer.remove();
+          });
         }
       });
     });
